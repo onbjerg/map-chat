cbf6826c038b53c08ae953960d6012bf3e80e939
Oliver Nordbjerg <oliver@pfmedia.dk>
2015-08-17 12:25:17 +0200

    Add default port fallback

--- server/server.js
+++ server/server.js
@@ -35,8 +35,9 @@ io.on('connection', function (socket) {
 
 app.use('/', express.static(__dirname + '/../client'))
 
-http.listen(process.env.PORT, function () {
-  console.log('listening on *:3000')
+var port = process.env.PORT || 3000
+http.listen(port, function () {
+  console.log('listening on *:' + port)
 })
 
 var stdin = process.openStdin()

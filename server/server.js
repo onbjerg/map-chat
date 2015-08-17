0e0d5be96b1142b5645185ed4a0a3ed54eb665f4
Oliver Nordbjerg <oliver@pfmedia.dk>
2015-08-17 07:15:13 +0200

    Implement greets and map

--- server/server.js
+++ server/server.js
@@ -7,12 +7,22 @@ var _ = require('underscore')
 io.on('connection', function (socket) {
   console.log('a user connected')
 
+  socket.on('greet', function (loc) {
+    console.log('a user greeted the server')
+    io.emit('greet', _.extend(loc, {
+      sessionID: this.id
+    }))
+  })
+
   socket.on('disconnect', function () {
     console.log('a user disconnected')
+    io.emit('leave', {
+      sessionID: this.id
+    })
   })
 
   socket.on('message', function (message) {
-    console.log('message from ' + this.id + ': ' + message)
+    console.log('message from ' + this.id + ': ', message)
     io.emit('message', _.extend(message, {
       sessionID: this.id
     }))
